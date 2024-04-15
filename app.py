0342c5a8557e796803799c3754fcb540e152324d
stanislav-chekmenev <stanislav.chekmenev@gmail.com>
2024-04-15 16:13:12 +0200

    pretify

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import gradio as gr
 from app_logic import estimate_net_salary
 
 # Create the slider for salary input.
-salary_slider = gr.components.Slider(minimum=80e3, maximum=200e3, label="Gross salary")
+salary_slider = gr.components.Slider(minimum=80e3, maximum=200e3, label="Gross annual salary")
 
 # Create switch inputs.
 self_employed = gr.components.Checkbox(label="Self-employed")
@@ -30,8 +30,8 @@ explanation = """
 
 ### Conclusion
 
-**Some minor and complex tax deductions were not taken into account. Based on the estimations above and my desire to earn 
-at around 4000 Euro a month, the projected self-employed salary would be around 160K Euro a year.**
+- Some minor and complex tax deductions were not taken into account. 
+- **Based on the estimations above and my desire to earn around 4000 Euro a month, the projected self-employed salary would be around 160K Euro a year.**
 
 ### How do I know the estimation is correct?
 
@@ -49,7 +49,7 @@ The estimation for the case of regular employment was tested against a German sa
 """
 
 # Create output interfaces.
-net_salary = gr.components.Textbox(label="Estimated net salary")
+net_salary = gr.components.Textbox(label="Estimated net annual salary")
 
 # Define the Gradio interface.
 iface = gr.Interface(
